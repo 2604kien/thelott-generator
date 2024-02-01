7bac92ec6f4daf930ce96aca6f562a78596e27fc
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2024-02-01 22:45:45 +1100

    lottery generator

---
+++ thelott.js
@@ -0,0 +1,18 @@
+function getLottNumber(n){
+    let array=[]
+    for(let i=0; i<n; i++){
+        let number;
+        number=Math.ceil(Math.random()*35);
+        while(array.includes(number)){
+            number=Math.ceil(Math.random()*35);
+        }
+        array.push(number);
+    }
+    return array;
+}
+function getPowerBall(){
+    return Math.ceil(Math.random()*20);
+}
+console.log("The Winning numbers is: ", getLottNumber(2));
+console.log("The P'ball is: ", getPowerBall());
+
